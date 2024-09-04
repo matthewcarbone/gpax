fa3ae0a1ede620135b2b2c192ed71d9958a41736
Matthew Carbone <x94carbone@gmail.com>
2024-09-04 16:51:59 -0400

    Update data Arrays and tests

---
+++ gpax/_tests/test_array.py
@@ -0,0 +1,4 @@
+from gpax.data import Array
+
+
+def test_Array(dummy_data): ...
--- gpax/data.py
+++ gpax/data.py
@@ -20,142 +20,6 @@ def ensure_2d(x):
 
 
 @define
-class Transform(ABC, MSONable):
-    _is_fit = field(default=False, validator=instance_of(bool))
-
-    @abstractmethod
-    def fit(self): ...
-
-    @abstractmethod
-    def forward(self): ...
-
-    @abstractmethod
-    def reverse(self): ...
-
-    @abstractmethod
-    def forward_std(self): ...
-
-    @abstractmethod
-    def reverse_std(self): ...
-
-
-@define
-class IdentityTransform(Transform):
-    def fit(self, _):
-        self._is_fit = True
-
-    def forward(self, x):
-        assert self._is_fit
-        return x
-
-    def reverse(self, x):
-        assert self._is_fit
-        return x
-
-    def forward_std(self, x_std):
-        assert self._is_fit
-        return x_std
-
-    def reverse_std(self, x_std):
-        assert self._is_fit
-        return x_std
-
-
-@define
-class ScaleTransform(Transform):
-    """Simple transform class for scaling data, provided in the shape of
-    N x n_dims, to minimum -1 and maximum 1 along each dimension."""
-
-    minima = field(default=None, validator=instance_of(DATA_TYPES))
-    maxima = field(default=None, validator=instance_of(DATA_TYPES))
-
-    def fit(self, x):
-        x = ensure_2d(x)
-        self._is_fit = True
-        if x is None:
-            return
-        self.minima = x.min(axis=0, keepdims=True)
-        self.maxima = x.max(axis=0, keepdims=True)
-
-    def forward(self, x):
-        assert self._is_fit
-        if x is None:
-            return None
-        x = ensure_2d(x)
-        numerator = x - self.minima
-        delta = self.maxima - self.minima + EPSILON
-        return (numerator / delta).copy()
-
-    def reverse(self, x):
-        assert self._is_fit
-        if x is None:
-            return None
-        x = ensure_2d(x)
-        delta = self.maxima - self.minima + EPSILON
-        return (x * delta + self.minima).copy()
-
-    def forward_std(self, x_std):
-        assert self._is_fit
-        if x_std is None:
-            return None
-        x_std = ensure_2d(x_std)
-        delta = self.maxima - self.minima + EPSILON
-        return (x_std / delta).copy()
-
-    def reverse_std(self, x_std):
-        assert self._is_fit
-        if x_std is None:
-            return None
-        x_std = ensure_2d(x_std)
-        delta = self.maxima - self.minima + EPSILON
-        return (x_std * delta).copy()
-
-
-@define
-class NormalizeTransform(Transform):
-    mean = field(default=None, validator=instance_of(DATA_TYPES))
-    std = field(default=None, validator=instance_of(DATA_TYPES))
-
-    def fit(self, x):
-        self._is_fit = True
-        if x is None:
-            return
-        x = ensure_2d(x)
-        self.mean = x.mean(axis=0, keepdims=True)
-        self.std = x.std(axis=0, keepdims=True)
-
-    def forward(self, x):
-        assert self._is_fit
-        if x is None:
-            return None
-        x = ensure_2d(x)
-        denominator = self.std + EPSILON
-        return ((x - self.mean) / denominator).copy()
-
-    def reverse(self, x):
-        assert self._is_fit
-        if x is None:
-            return None
-        x = ensure_2d(x)
-        denominator = self.std + EPSILON
-        return (x * denominator + self.mean).copy()
-
-    def forward_std(self, x_std):
-        assert self._is_fit
-        if x_std is None:
-            return None
-        x_std = ensure_2d(x_std)
-        return (x_std / self.std).copy()
-
-    def reverse_std(self, x_std):
-        assert self._is_fit
-        if x_std is None:
-            return None
-        x_std = ensure_2d(x_std)
-        return (x_std * self.std).copy()
-
-
-@define
 class ArrayMetadata(MSONable):
     transforms_as = field(default="mean", validator=instance_of(str))
     is_transformed = field(default=False, validator=instance_of(bool))
@@ -198,3 +62,163 @@ class Array(MSONable, np.ndarray):
     def to_json(self):
         d = self.as_dict()
         return json.dumps(d, cls=MontyEncoder)
+
+
+@define
+class Transform(ABC, MSONable):
+    _is_fit = field(default=False, validator=instance_of(bool))
+
+    @abstractmethod
+    def fit(self): ...
+
+    @abstractmethod
+    def _forward_mean(self): ...
+
+    @abstractmethod
+    def _forward_std(self): ...
+
+    @abstractmethod
+    def _reverse_mean(self): ...
+
+    @abstractmethod
+    def _reverse_std(self): ...
+
+    def _preprocess(self, x, fitting=False):
+        """Preprocesses the input to a forward or reverse transformation.
+
+        Parameters
+        ----------
+        x : array_like
+            The input array to transform.
+        fitting : bool
+            Whether or not the function calling this one is fitting the
+            transform or performing a transform.
+
+        Returns
+        -------
+        array_like, None
+        """
+
+        if not self._is_fit and not fitting:
+            raise RuntimeError("Transform must be fit before forward/reverse")
+        if self._is_fit and fitting:
+            raise RuntimeError("Cannot fit the transform twice")
+        if x is None:
+            return None
+        if not hasattr(x, "metadata"):
+            raise ValueError("Input array must be of type Array")
+        x = ensure_2d(x)
+        return x
+
+    def forward(self, x):
+        x = self._preprocess(x, False)
+        if x is None:
+            return None
+        if x.metadata.is_transformed:
+            return x
+        t_as = x.metadata.transforms_as
+        if t_as == "mean":
+            new_x = self._forward_mean(x)
+        elif t_as == "std":
+            new_x = self._forward_std(x)
+        else:
+            raise ValueError(f"Array has invalid transform type {t_as}")
+        new_x.metadata.is_transformed = True
+        return new_x
+
+    def reverse(self, x):
+        x = self._preprocess(x, False)
+        if x is None:
+            return None
+        if not x.metadata.is_transformed:
+            return x
+        t_as = x.metadata.transforms_as
+        if t_as == "mean":
+            new_x = self._reverse_mean(x)
+        elif t_as == "std":
+            new_x = self._reverse_std(x)
+        else:
+            raise ValueError(f"Array has invalid transform type {t_as}")
+        new_x.metadata.is_transformed = False
+        return new_x
+
+
+@define
+class IdentityTransform(Transform):
+    def fit(self, x):
+        self._preprocess(x, True)
+        self._is_fit = True
+
+    def _forward_mean(self, x):
+        return x
+
+    def _forward_std(self, x):
+        return x
+
+    def _reverse_mean(self, x):
+        return x
+
+    def _reverse_std(self, x):
+        return x
+
+
+@define
+class ScaleTransform(Transform):
+    """Simple transform class for scaling data, provided in the shape of
+    N x n_dims, to minimum -1 and maximum 1 along each dimension."""
+
+    minima = field(default=None, validator=instance_of(DATA_TYPES))
+    maxima = field(default=None, validator=instance_of(DATA_TYPES))
+
+    def fit(self, x):
+        x = self._preprocess(x, True)
+        if x is None:
+            return
+        self._is_fit = True
+        self.minima = x.min(axis=0, keepdims=True)
+        self.maxima = x.max(axis=0, keepdims=True)
+
+    def _forward_mean(self, x):
+        numerator = x - self.minima
+        delta = self.maxima - self.minima + EPSILON
+        return numerator / delta
+
+    def _forward_std(self, x):
+        delta = self.maxima - self.minima + EPSILON
+        return x / delta
+
+    def _reverse_mean(self, x):
+        delta = self.maxima - self.minima + EPSILON
+        return x * delta + self.minima
+
+    def _reverse_std(self, x):
+        delta = self.maxima - self.minima + EPSILON
+        return x * delta
+
+
+@define
+class NormalizeTransform(Transform):
+    mean = field(default=None, validator=instance_of(DATA_TYPES))
+    std = field(default=None, validator=instance_of(DATA_TYPES))
+
+    def fit(self, x):
+        x = self._preprocess(x, True)
+        if x is None:
+            return
+        self._is_fit = True
+        self.mean = x.mean(axis=0, keepdims=True)
+        self.std = x.std(axis=0, keepdims=True)
+
+    def _forward_mean(self, x):
+        denominator = self.std + EPSILON
+        return (x - self.mean) / denominator
+
+    def _forward_std(self, x):
+        return x / self.std
+
+    def _reverse_mean(self, x):
+        denominator = self.std + EPSILON
+        return x * denominator + self.mean
+
+    def _reverse_std(self, x):
+        return x * self.std
