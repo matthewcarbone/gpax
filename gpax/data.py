fdb111491df90f75ebc860b52e1e718f81c71656
Matthew Carbone <x94carbone@gmail.com>
2024-09-04 14:30:16 -0400

    Add gpax.monty and serialization ability for Array object

---
+++ gpax/data.py
@@ -0,0 +1,200 @@
+import json
+from abc import ABC, abstractmethod
+from copy import deepcopy
+
+import jax.numpy as jnp
+import numpy as np
+from attrs import define, field
+from attrs.validators import instance_of
+
+from gpax.monty.json import MontyDecoder, MontyEncoder, MSONable
+
+EPSILON = 1e-8
+DATA_TYPES = (jnp.ndarray, np.ndarray, type(None))
+
+
+def ensure_2d(x):
+    if x.ndim == 1:
+        x = x.reshape(1, -1)
+    return x
+
+
+@define
+class Transform(ABC, MSONable):
+    _is_fit = field(default=False, validator=instance_of(bool))
+
+    @abstractmethod
+    def fit(self): ...
+
+    @abstractmethod
+    def forward(self): ...
+
+    @abstractmethod
+    def reverse(self): ...
+
+    @abstractmethod
+    def forward_std(self): ...
+
+    @abstractmethod
+    def reverse_std(self): ...
+
+
+@define
+class IdentityTransform(Transform):
+    def fit(self, _):
+        self._is_fit = True
+
+    def forward(self, x):
+        assert self._is_fit
+        return x
+
+    def reverse(self, x):
+        assert self._is_fit
+        return x
+
+    def forward_std(self, x_std):
+        assert self._is_fit
+        return x_std
+
+    def reverse_std(self, x_std):
+        assert self._is_fit
+        return x_std
+
+
+@define
+class ScaleTransform(Transform):
+    """Simple transform class for scaling data, provided in the shape of
+    N x n_dims, to minimum -1 and maximum 1 along each dimension."""
+
+    minima = field(default=None, validator=instance_of(DATA_TYPES))
+    maxima = field(default=None, validator=instance_of(DATA_TYPES))
+
+    def fit(self, x):
+        x = ensure_2d(x)
+        self._is_fit = True
+        if x is None:
+            return
+        self.minima = x.min(axis=0, keepdims=True)
+        self.maxima = x.max(axis=0, keepdims=True)
+
+    def forward(self, x):
+        assert self._is_fit
+        if x is None:
+            return None
+        x = ensure_2d(x)
+        numerator = x - self.minima
+        delta = self.maxima - self.minima + EPSILON
+        return (numerator / delta).copy()
+
+    def reverse(self, x):
+        assert self._is_fit
+        if x is None:
+            return None
+        x = ensure_2d(x)
+        delta = self.maxima - self.minima + EPSILON
+        return (x * delta + self.minima).copy()
+
+    def forward_std(self, x_std):
+        assert self._is_fit
+        if x_std is None:
+            return None
+        x_std = ensure_2d(x_std)
+        delta = self.maxima - self.minima + EPSILON
+        return (x_std / delta).copy()
+
+    def reverse_std(self, x_std):
+        assert self._is_fit
+        if x_std is None:
+            return None
+        x_std = ensure_2d(x_std)
+        delta = self.maxima - self.minima + EPSILON
+        return (x_std * delta).copy()
+
+
+@define
+class NormalizeTransform(Transform):
+    mean = field(default=None, validator=instance_of(DATA_TYPES))
+    std = field(default=None, validator=instance_of(DATA_TYPES))
+
+    def fit(self, x):
+        self._is_fit = True
+        if x is None:
+            return
+        x = ensure_2d(x)
+        self.mean = x.mean(axis=0, keepdims=True)
+        self.std = x.std(axis=0, keepdims=True)
+
+    def forward(self, x):
+        assert self._is_fit
+        if x is None:
+            return None
+        x = ensure_2d(x)
+        denominator = self.std + EPSILON
+        return ((x - self.mean) / denominator).copy()
+
+    def reverse(self, x):
+        assert self._is_fit
+        if x is None:
+            return None
+        x = ensure_2d(x)
+        denominator = self.std + EPSILON
+        return (x * denominator + self.mean).copy()
+
+    def forward_std(self, x_std):
+        assert self._is_fit
+        if x_std is None:
+            return None
+        x_std = ensure_2d(x_std)
+        return (x_std / self.std).copy()
+
+    def reverse_std(self, x_std):
+        assert self._is_fit
+        if x_std is None:
+            return None
+        x_std = ensure_2d(x_std)
+        return (x_std * self.std).copy()
+
+
+@define
+class ArrayMetadata(MSONable):
+    transforms_as = field(default="mean", validator=instance_of(str))
+    is_transformed = field(default=False, validator=instance_of(bool))
+
+
+class Array(MSONable, np.ndarray):
+    def __new__(cls, input_array):
+        if hasattr(input_array, "metadata"):
+            metadata = deepcopy(input_array.metadata)
+        else:
+            metadata = deepcopy(ArrayMetadata())
+        obj = np.asarray(input_array).view(cls)  # this goes to finalize
+        obj.metadata = deepcopy(metadata)
+        return obj
+
+    def __array_finalize__(self, obj):
+        if obj is None:
+            return
+        self.metadata = getattr(obj, "metadata", None)
+
+    def as_dict(self):
+        d = super().as_dict()
+        arr = np.array(self)
+        return {
+            **d,
+            "!array_data": arr,
+            "!metadata": self.metadata,
+        }
+
+    @classmethod
+    def from_dict(cls, d):
+        d = d.copy()
+        d = {k: v for k, v in d.items() if not k.startswith("@")}
+        data = MontyDecoder().process_decoded(d.pop("!array_data"))
+        klass = cls(data)
+        metadata = d.pop("!metadata")
+        klass.metadata = deepcopy(ArrayMetadata.from_dict(metadata))
+        return klass
+
+    def to_json(self):
+        d = self.as_dict()
+        return json.dumps(d, cls=MontyEncoder)
