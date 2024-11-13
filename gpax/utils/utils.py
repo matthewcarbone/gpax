f6cf8064f370397d208c3afb44bd432f99ed76b8
Matthew Carbone <x94carbone@gmail.com>
2024-11-12 20:54:55 -0500

    Completely document utils.utils

--- gpax/utils/utils.py
+++ gpax/utils/utils.py
@@ -1,55 +1,75 @@
 """
-utils.py
-========
-
-Utility functions
-
-Created by Maxim Ziatdinov (email: maxim.ziatdinov@ai4microscopy.com)
-Modified by Matthew R. Carbone (email: x94carbone@gmail.com)
+Utility functions used in GPax.
 """
 
-import sys
-import warnings
-from contextlib import contextmanager
-from functools import wraps
+# Created by Maxim Ziatdinov (email: maxim.ziatdinov@ai4microscopy.com)
+# Modified by Matthew R. Carbone (email: x94carbone@gmail.com)
+
+from collections.abc import Iterable
 from itertools import product
 from time import perf_counter
+from typing import Any
 
 import jax
 import jax.numpy as jnp
+import numpy as np
+from numpy.typing import ArrayLike
 
-from gpax import state
 
+def enable_x64() -> None:
+    """Use double (x64) precision for jax arrays
 
-def enable_x64():
-    """Use double (x64) precision for jax arrays"""
+    :::{important}
+    Due to how sensitive the kernel operations can be on the floating point
+    precision, x64 is enabled automatically whenever {py:class}`gpax.gp` is
+    imported. If you want to disable this behavior, you can do so manually via
+
+    ```python
+    import jax
+
+    jax.config.update("jax_enable_x64", False)
+    ```
+    :::
+    """
+
     jax.config.update("jax_enable_x64", True)
 
 
-def split_array(x, s=100):
-    """Splits an array along its 0th axis into parts [roughly] equal to the
-    provided batch size, s."""
+def split_array(x: Iterable, s: int = 100) -> list[Any]:
+    """Splits an iterable `x` along its 0th axis into parts [roughly] equal to
+    the provided batch size, `s`
+
+    :param x:
+        The iterable to split along its 0th axis.
+    :param s:
+        The batch size. This is the approximate size each of the new list
+        chunks will be.
+
+    :returns: A new list in which the provided one has been split.
+    """
 
     return [x[i : i + s, ...] for i in range(0, len(x), s)]
 
 
-def scale_by_domain(x, domain):
-    """Scales provided data x by the bounds provided in the domain. Note that
-    all dimensions must perfectly match.
+def scale_by_domain(x: ArrayLike, domain: ArrayLike) -> ArrayLike:
+    """Scales provided data `x` by the bounds provided in the `domain`
 
-    Parameters
-    ----------
-    x : array_like
-        The input data of shape (N, d). This data should be scaled between 0
-        and 1.
-    domain : array_like
-        The domain to scale to. Should be of shape (2, d), where domain[0, :]
-        is the minimum along each axis and domain[1, :] is the maximum.
+    :::{note}
+    All dimensions must *perfectly* match.
+    :::
 
-    Returns
-    -------
-    array_like
-        The scaled data of shape (N, d).
+    :param x:
+        The input data of shape `(N, d)`, scaled between 0 and 1 along all
+        dimensions.
+    :param domain:
+        The domain to scale to. Should be of shape `(2, d)`, where
+        `domain[0, :]` is the minimum along each axis and `domain[1, :]`
+        is the maximum.
+
+    :returns: The scaled data of shape `(N, d)`.
+
+    :raises ValueError: If any dimensions do not match, or if the input data
+    is not scaled properly between 0 and 1 along all dimensions.
     """
 
     if x.ndim != 2:
@@ -70,21 +90,18 @@ def scale_by_domain(x, domain):
     return (domain[1, :] - domain[0, :]) * x + domain[0, :]
 
 
-def get_coordinates(points_per_dimension, domain):
-    """Gets a grid of equally spaced points on each dimension.
-    Returns these results in coordinate representation.
+def get_coordinates(
+    points_per_dimension: int | list, domain: np.typing.ArrayLike
+) -> np.typing.ArrayLike:
+    """Gets a grid of equally spaced points on each dimension and returns
+    these results in coordinate representation
 
-    Parameters
-    ----------
-    points_per_dimension : int or list
+    :param points_per_dimension:
         The number of points per dimension. If int, assumed to be 1d.
-    domain : array_like
-        A 2 x d array indicating the domain along each axis.
+    :param domain:
+        A shape `(2, d)` array indicating the domain along each axis.
 
-    Returns
-    -------
-    jnp.ndarray
-        The available points for sampling.
+    :returns: The available points for sampling.
     """
 
     if isinstance(points_per_dimension, int):
@@ -94,6 +111,19 @@ def get_coordinates(points_per_dimension, domain):
 
 
 class Timer:
+    """A simple timer context manager
+
+    # Usage
+
+    ```python
+    import time
+
+    with Timer() as timer:
+        time.sleep(5)
+    print(f"done in {timer.elapsed:.02f} seconds")
+    ```
+    """
+
     def __enter__(self):
         self.t0 = perf_counter()
         return self
@@ -103,66 +133,66 @@ class Timer:
         self.elapsed = tf - self.t0
 
 
-def time_function(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        with Timer() as timer:
-            result = func(*args, **kwargs)
-        if state.verbose > 1:
-            print(f"{func.__qualname__} took {timer.elapsed:.02e} s")
-        return result
+# def time_function(func):
+#     @wraps(func)
+#     def wrapper(*args, **kwargs):
+#         with Timer() as timer:
+#             result = func(*args, **kwargs)
+#         if state.verbose > 1:
+#             print(f"{func.__qualname__} took {timer.elapsed:.02e} s")
+#         return result
+#
+#     return wrapper
+#
 
-    return wrapper
+# def delay_warnings(func):
+#     @wraps(func)
+#     def wrapper(*args, **kwargs):
+#         with warnings.catch_warnings(record=True) as captured_warnings:
+#             warnings.simplefilter("always")
+#             result = func(*args, **kwargs)
+#         seen_warnings = set()
+#         unique_warnings = []
+#         for w in captured_warnings:
+#             key = (str(w.message), w.category, w.filename, w.lineno, w.line)
+#             if key not in seen_warnings:
+#                 unique_warnings.append(w)
+#         for w in unique_warnings:
+#             warnings.showwarning(
+#                 message=w.message,
+#                 category=w.category,
+#                 filename=w.filename,
+#                 lineno=w.lineno,
+#                 file=sys.stderr,
+#                 line=w.lilne,
+#             )
+#         return result
+#
+#     return wrapper
 
 
-def delay_warnings(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        with warnings.catch_warnings(record=True) as captured_warnings:
-            warnings.simplefilter("always")
-            result = func(*args, **kwargs)
-        seen_warnings = set()
-        unique_warnings = []
-        for w in captured_warnings:
-            key = (str(w.message), w.category, w.filename, w.lineno, w.line)
-            if key not in seen_warnings:
-                unique_warnings.append(w)
-        for w in unique_warnings:
-            warnings.showwarning(
-                message=w.message,
-                category=w.category,
-                filename=w.filename,
-                lineno=w.lineno,
-                file=sys.stderr,
-                line=w.lilne,
-            )
-        return result
-
-    return wrapper
-
-
-def dict_of_list_to_list_of_dict(d):
-    """Emulates a lot of the functionality of jax.vmap but in pure Python.
-    Converts a dictionary of arrays to a list of dictionaries in which each
-    key of the resultant dictionaries contains only a single value. For
-    example, converts
-
-    data = {
-        'a': [1, 2, 3],
-        'b': [4, 5, 6],
-        'c': [7, 8, 9],
-    }
-
-    to
-
-    [
-        {'a': 1, 'b': 4, 'c': 7},
-        {'a': 2, 'b': 5, 'c': 8},
-        {'a': 3, 'b': 6, 'c': 9}
-    ]
-    """
-
-    k = d.keys()
-    v = zip(*d.values())
-    for vv in v:
-        yield dict(zip(k, vv))
+# def dict_of_list_to_list_of_dict(d):
+#     """Emulates a lot of the functionality of jax.vmap but in pure Python.
+#     Converts a dictionary of arrays to a list of dictionaries in which each
+#     key of the resultant dictionaries contains only a single value. For
+#     example, converts
+#
+#     data = {
+#         'a': [1, 2, 3],
+#         'b': [4, 5, 6],
+#         'c': [7, 8, 9],
+#     }
+#
+#     to
+#
+#     [
+#         {'a': 1, 'b': 4, 'c': 7},
+#         {'a': 2, 'b': 5, 'c': 8},
+#         {'a': 3, 'b': 6, 'c': 9}
+#     ]
+#     """
+#
+#     k = d.keys()
+#     v = zip(*d.values())
+#     for vv in v:
+#         yield dict(zip(k, vv))
