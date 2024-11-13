2574d85c006872f0f0b1d41e2caa3d7b77236543
Matthew Carbone <x94carbone@gmail.com>
2024-11-12 20:21:45 -0500

    Fully document prior_utils

--- gpax/utils/prior_utils.py
+++ gpax/utils/prior_utils.py
@@ -1,44 +1,54 @@
-"""
-prior_utils.py
-==============
-
-Utilities for priors and probabilistic functions used in numpyro. Specifically
+"""Utilities for priors and probabilistic functions used in numpyro. Specifically
 contains abstractions over mean and kernel functions in the form of the
-``Prior`` object.
-
-Created by Matthew R. Carbone (email: x94carbone@gmail.com)
+{py:class}`Prior` object.
 """
+
+# Created by Matthew R. Carbone (email: x94carbone@gmail.com)
 
 from abc import ABC, abstractproperty
-from functools import cached_property
+from functools import cache
+from typing import Union
 
 import jax.numpy as jnp
 import numpyro
 import numpyro.distributions as dist
-from attrs import define, field
-from attrs.validators import ge, instance_of
 from monty.json import MSONable
 
 
-def get_parameter_prior(name, distribution, plate=1):
-    """A utility function for getting a numpyro prior from a provided
-    distribution. Floats and ints are interpreted as deterministic values.
+def get_parameter_prior(
+    name: str,
+    distribution: Union[dist.Distribution, float, int],
+    plate: int = 1,
+):
+    """A utility function for getting a NumPyro prior from a provided
+    distribution
 
-    Parameters
-    ----------
-    name : str
-        The name of the variable
-    distribution : numpyro.distributions.Distribution or float or int
+    Provides the NumPyro prior via {py:class}`numpyro.sample` from the
+    distribution. Also accounts for if `plate>1`, in which case `plate`
+    independent samples are drawn for that parameter. `float` and `int` are
+    interpreted as deterministic values.
+
+    :::{warning}
+    This function should only be used within a NumPyro model. Otherwise, an
+    error will be thrown.
+    :::
+
+    :::{note}
+    For more details on how {py:class}`numpyro.sample` works, see the
+    [NumPyro documentation](https://num.pyro.ai/en/latest/primitives.html#sample).
+    :::
+
+    :param name:
+        The name of the variable.
+    :param distribution:
         The prior distribution to sample from. This is interpreted as
         numpyro.deterministic if float or int.
-    plate : int
+    :param plate:
         The number of dimensions to run the numpyro plate primitive over.
         Defaults to 1.
 
-    Returns
-    -------
-    Some numpyro object depending on whether or not distribution is a true
-    distribution or just some constant value.
+    :returns: Some numpyro object depending on whether or not distribution is
+    a true distribution or just some constant value.
     """
 
     if isinstance(distribution, (float, int)):
@@ -59,22 +69,19 @@ def get_parameter_prior(name, distribution, plate=1):
         return numpyro.sample(name, distribution)
 
 
-def get_parameter_as_float(parameter):
+def distribution_as_float(parameter: dist.Distribution) -> float:
     """Converts a provided parameter, potentially a numpyro distribution,
-    into a 'reduced' representation. This will either be simply the provided
-    parameter if constant (float or int), or the mean of a distribution if
-    parameter is of type numpyro.distributions.Distribution.
+    into a scalar representation
 
-    Parameters
-    ----------
-    parameter : numpyro.distribution, int, float
+    Converts a parameter into a float. This will either be simply the provided
+    parameter if constant (`float` or `int`), or the mean of a distribution if
+    parameter is of type {py:obj}`numpyro.distributions.Distribution`.
+
+    :param parameter:
         The parameter to retrieve in float representation.
 
-    Returns
-    -------
-    float
-        The mean of the input parameter distribution (or simply the value if
-        the input is a constant float or int).
+    :returns: The mean of the input parameter distribution (or simply the
+    value if the input is a constant float or int).
     """
 
     if isinstance(parameter, numpyro.distributions.Distribution):
@@ -82,36 +89,53 @@ def get_parameter_as_float(parameter):
     return parameter
 
 
-@define
 class Parameter(MSONable):
-    """Object containing a single parameter for a probabilistic model.
+    """A parameter used in distribution
+
+    Object containing a single parameter for a probabilistic model.
     Specifically, contains both the distribution (or constant) itself, as well
     as a value for ``plate``, which represents the number of independent
     instances of a variable to use during inference/training.
 
-    Parameters
-    ----------
-    value : numpyro.distribution, int, float
+    :param value:
         The value of the parameter itself, either a distribution or float/int.
-    plate : int
+    :param plate:
         The number of independent instances of the variable. Used with
         numpyro.plate.
     """
 
-    value = field(validator=instance_of((dist.Distribution, int, float)))
-    plate = field(default=1, validator=[instance_of(int), ge(1)])
+    def __init__(
+        self, value: Union[dist.Distribution, int, float], plate: int = 1
+    ):
+        self.value = value
+        self.plate = plate
 
     def _get_parameter_prior(self, name):
         return get_parameter_prior(name, self.value, self.plate)
 
 
-@define
 class Prior(ABC, MSONable):
-    """Base class representing any prior used in GPax. This can be, for
-    instance, a mean prior, or a kernel prior, used in a Gaussian Process."""
+    """A prior abstract base class
+
+    Base class representing any prior used in GPax. This can be, for
+    instance, a mean prior, or a kernel prior, used in a Gaussian Process.
+
+    :::{warning}
+    Variable naming for the {py:class}`Prior` is very important. Any variable
+    which does not begin with the {py:obj}`attribute_prefix` string will
+    not be considered during inference.
+
+    For example, in the {py:obj}`gpax.kernels.Kernel` objects, this prefix
+    is set to `"k_"`. Thus, parameters like `k_scale` are considered during
+    inference, whereasa a parameter such as `scale` will not be considered.
+    :::
+    """
 
     @abstractproperty
-    def attribute_prefix(self):
+    def attribute_prefix(self) -> str:
+        """Class attributes that start with this prefix will be considered
+        during GP training."""
+
         raise NotImplementedError
 
     def _params(self):
@@ -125,7 +149,8 @@ class Prior(ABC, MSONable):
                 params[key] = obj
         return params
 
-    @cached_property
+    @cache
+    @property
     def params(self):
         """Returns a dictionary, indexed by the parameter name, of all
         prior parameters available to the model. The parameters are extracted
@@ -152,17 +177,21 @@ class Prior(ABC, MSONable):
         a constant)."""
 
         return {
-            k: get_parameter_as_float(v.value) for k, v in self._params.items()
+            k: distribution_as_float(v.value) for k, v in self._params.items()
         }
 
 
-@define
 class MeanPrior(Prior):
-    def attribute_prefix(self):
+    """Simple mean prior which sets the {py:obj}`attribute_prefix` to `"m_"`"""
+
+    @property
+    def attribute_prefix(self) -> str:
         return "m_"
 
 
-@define
 class KernelPrior(Prior):
-    def attribute_prefix(self):
+    """Simple mean prior which sets the {py:obj}`attribute_prefix` to `"k_"`"""
+
+    @property
+    def attribute_prefix(self) -> str:
         return "k_"
