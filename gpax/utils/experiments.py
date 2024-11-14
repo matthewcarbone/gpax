cb14f335ec4b0d1b4352038cf8cd9d88d7fee25d
Matthew Carbone <x94carbone@gmail.com>
2024-11-14 15:08:25 -0500

    Remove attrs dependency from experiments

--- gpax/utils/experiments.py
+++ gpax/utils/experiments.py
@@ -1,66 +1,142 @@
 """
-experiments.py
-==============
-
 Module containing an Experiment abstraction as well as a few "dummy" datasets
 which can be used for testing and demonstrating GP-based Bayesian optimization
 (or really any optimization).
-
-Created by Matthew R. Carbone (email: x94carbone@gmail.com)
-
-BSD 3-Clause License
-
-Copyright (c) 2022, Brookhaven Science Associates, LLC, Brookhaven National Laboratory
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-
-3. Neither the name of the copyright holder nor the names of its contributors
-   may be used to endorse or promote products derived from this software
-   without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
+# Created by Matthew R. Carbone (email: x94carbone@gmail.com)
+#
+# BSD 3-Clause License
+#
+# Copyright (c) 2022, Brookhaven Science Associates, LLC, Brookhaven National Laboratory
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import warnings
-from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod, abstractproperty
+from dataclasses import dataclass
+from functools import cache
 from itertools import product
+from typing import Any
 
 import numpy as np
-from attrs import define, field, frozen
-from attrs.validators import ge, instance_of, optional
 from monty.json import MSONable
+from numpy.typing import ArrayLike
 from scipy.stats import qmc
 
 
-@frozen
+@dataclass(frozen=True)
 class ExperimentProperties(MSONable):
     """Defines the core set of experiment properties, which are frozen after
     setting. These are also serializable and cannot be changed after they
-    are set."""
+    are set.
 
-    n_input_dim = field(validator=instance_of(int))
-    n_output_dim = 1
-    domain = field(validator=optional(instance_of(np.ndarray)))
+    :::{note}
+    Generally, it should not be required to deal with this class unless
+    constructing your own experiments.
+    :::
+    """
+
+    _n_input_dim: int
+    _n_output_dim: int = 1
+    _domain: ArrayLike | None = None
+
+    def _validate_n_input_dim(self):
+        x = self.n_input_dim
+        if not isinstance(x, int):
+            raise ValueError(f"n_input_dim ({x}) must be of type int")
+        if not isinstance(x >= 1):
+            raise ValueError(f"n_input_dim ({x}) must be >= 1")
+
+    def _validate_n_output_dim(self):
+        x = self.n_output_dim
+        if not isinstance(x, int):
+            raise ValueError(f"n_output_dim ({x}) must be of type int")
+        if not isinstance(x >= 1):
+            raise ValueError(f"n_output_dim ({x}) must be >= 1")
+
+    def _validate_domain(self):
+        if self.domain is None:
+            return
+        x = self.domain
+        if not x.ndim == 2 or x.shape[0] != 2:
+            raise ValueError("domain must be of shape `(2, d)`")
+        if np.any(x[0, :] >= x[1, :]):
+            raise ValueError("domain[0, :] must be < domain[1, :]")
+
+    def __post_init__(self):
+        self._validate_n_input_dim()
+        self._validate_n_output_dim()
+        self._validate_domain()
+
+    @property
+    def n_input_dim(self) -> int:
+        """The number of input dimensions to the experiment. In other words,
+        the input to the experiment should always be of shape `(N, d)`,
+        where `d` is `n_input_dim`."""
+        return self._n_input_dim
+
+    @n_input_dim.setter
+    def n_input_dim(self, x: int):
+        self._n_input_dim = x
+        self._validate_n_input_dim()
+
+    @property
+    def n_output_dim(self) -> int:
+        """The number of output dimensions from the experiment. Generally,
+        this should be 1, indicating that the output will be a
+        1-dimensional vector of shape `(N,)` or `(N, 1)`. However, in
+        principle, there is no reason `n_output_dim` cannot be some
+        number greater than 1. In this case, each observation is a vector,
+        such as a spectrum or image."""
+
+        return self._n_output_dim
+
+    @n_output_dim.setter
+    def n_output_dim(self, x: int):
+        self._n_output_dim = x
+        self._validate_n_output_dim()
+
+    @property
+    def domain(self) -> ArrayLike | None:
+        """The domain of validity for the experiment. Should be an
+        {py:obj}`ArrayLike` object of shape `(2, d)`, where `d` is the
+        number of input dimensions given by `n_input_dim`. Each column
+        represents the minimum and maximum along that dimension."""
+
+        return self._domain
+
+    @domain.setter
+    def domain(self, x: ArrayLike):
+        self._domain = x
+        self._validate_domain()
 
 
+# TODO: docs
 def scale_by_domain(x, domain):
     """Scales provided data x by the bounds provided in the domain. Note that
     all dimensions must perfectly match.
@@ -98,6 +174,7 @@ def scale_by_domain(x, domain):
     return (domain[1, :] - domain[0, :]) * x + domain[0, :]
 
 
+# TODO: docs
 def get_coordinates(points_per_dimension, domain):
     """Gets a grid of equally spaced points on each dimension.
     Returns these results in coordinate representation.
@@ -121,6 +198,7 @@ def get_coordinates(points_per_dimension, domain):
     return scale_by_domain(np.array([xx for xx in gen]), domain)
 
 
+# TODO: docs
 def get_random_points(domain, n=1):
     """Gets a random selection of points on a provided domain. The dimension
     of the data is inferred from the shape of the domain.
@@ -141,6 +219,7 @@ def get_random_points(domain, n=1):
     return scale_by_domain(X, domain)
 
 
+# TODO: docs
 def get_latin_hypercube_points(domain, n=5, seed=None):
     """Gets a random selection of points in the provided domain using the
     Latin Hypercube sampling algorithm.
@@ -164,7 +243,8 @@ def get_latin_hypercube_points(domain, n=5, seed=None):
     return qmc.scale(sample, *domain)
 
 
-@define
+# TODO: docs
+@dataclass
 class Experiment(ABC, MSONable):
     """Abstract base class for an experiment, which can be loosely thought of
     as a class which has a single useful method: __call__. A call to the
@@ -172,8 +252,14 @@ class Experiment(ABC, MSONable):
     x. The output is always a (N,)-shape array containing the scalar
     observations at each point x."""
 
-    metadata = field(factory=dict)
-    properties = field(default=None)
+    metadata: dict[str, Any] | None = None
+
+    @abstractproperty
+    def properties(self) -> ExperimentProperties:
+        raise NotImplementedError
+
+    def __post_init__(self):
+        self.metadata = self.metadata or {}
 
     @abstractmethod
     def _truth(self, _):
@@ -277,7 +363,7 @@ class Experiment(ABC, MSONable):
         return self.truth(x)
 
 
-@define
+# TODO: docs
 class ErrorbarExperiment(Experiment):
     """Identical to the Experiment, except that the result of __call__ not
     only returns the value of the experiment but also a single standard
@@ -300,14 +386,15 @@ class ErrorbarExperiment(Experiment):
         return self.truth(x), self.uncertainty(x)
 
 
-@define
 class SimpleSinusoidal1d(Experiment):
-    properties = field(
-        factory=lambda: ExperimentProperties(
+    noise: float | int = 0.1
+
+    @cache
+    @property
+    def properties(self) -> ExperimentProperties:
+        return ExperimentProperties(
             n_input_dim=1, domain=np.array([-1.1, 1.1]).reshape(-1, 1)
         )
-    )
-    noise = field(default=0.1, validator=[instance_of((float, int)), ge(0.0)])
 
     # FIX: this is a problem! You forgot the noise!!!
     def _truth(self, x):
@@ -324,29 +411,29 @@ class SimpleSinusoidal1d(Experiment):
         return x, y
 
 
-@define
 class SimpleDecayingSinusoidal1d(Experiment):
-    properties = field(
-        factory=lambda: ExperimentProperties(
+    @cache
+    @property
+    def properties(self) -> ExperimentProperties:
+        return ExperimentProperties(
             n_input_dim=1, domain=np.array([-3.0, 3.0]).reshape(-1, 1)
         )
-    )
 
     def _truth(self, x):
         y = np.sin(5 * x + np.pi / 2) * np.exp(-(x**2))
         return np.atleast_1d(y.squeeze())
 
 
-@define
 class Simple2d(Experiment):
-    properties = field(
-        factory=lambda: ExperimentProperties(
+    true_optima: ArrayLike = np.array([2.0, -4.0])
+
+    @cache
+    @property
+    def properties(self) -> ExperimentProperties:
+        return ExperimentProperties(
             n_input_dim=2,
             domain=np.array([[-4.0, 5.0], [-5.0, 4.0]]).T,
         )
-    )
-
-    true_optima = np.array([2.0, -4.0])
 
     def _truth(self, x):
         x1 = x[:, 0]
@@ -360,10 +447,7 @@ class Simple2d(Experiment):
         return np.atleast_1d(y.squeeze())
 
 
-E_GRID = np.linspace(-1, 1, 100)
-
-
-def _mu_Gaussians(p, E=E_GRID, x0=0.5, sd=0.05):
+def _mu_Gaussians(p, E=np.linspace(-1, 1, 100), x0=0.5, sd=0.05):
     """Returns a dummy "spectrum" which is just two Gaussian functions. The
     proportion of the two functions is goverened by ``p``.
 
@@ -417,18 +501,19 @@ def _get_phase_from_proportion(x, x0, a, gaussian_x0, gaussian_sd):
     )
 
 
-@define
 class Sine2Phase(Experiment):
-    properties = field(
-        factory=lambda: ExperimentProperties(
+    x0: float = 0.5
+    a: float = 100.0
+    gaussian_x0: float = 0.5
+    gaussian_sd: float = 0.22
+
+    @cache
+    @property
+    def properties(self) -> ExperimentProperties:
+        return ExperimentProperties(
             n_input_dim=2,
             domain=np.array([[0.0, 1.0], [0.0, 1.0]]).T,
         )
-    )
-    x0 = field(default=0.5)
-    a = field(default=100.0)
-    gaussian_x0 = field(default=0.5)
-    gaussian_sd = field(default=0.22)
 
     def get_phase(self, x):
         return np.array(
@@ -443,31 +528,38 @@ class Sine2Phase(Experiment):
 
 # https://www.sfu.ca/~ssurjano/branin.html
 
-BRANIN_A = 1.0
-BRANIN_B = 5.1 / (4.0 * np.pi**2)
-BRANIN_C = 5.0 / np.pi
-BRANIN_R = 6.0
-BRANIN_S = 10.0
-BRANIN_T = 1.0 / (8.0 * np.pi)
-BRANIN_OPTIMA = np.array([[-np.pi, 12.275], [np.pi, 2.275], [9.42478, 2.475]])
+_BRANIN_A = 1.0
+_BRANIN_B = 5.1 / (4.0 * np.pi**2)
+_BRANIN_C = 5.0 / np.pi
+_BRANIN_R = 6.0
+_BRANIN_S = 10.0
+_BRANIN_T = 1.0 / (8.0 * np.pi)
+_BRANIN_OPTIMA = np.array([[-np.pi, 12.275], [np.pi, 2.275], [9.42478, 2.475]])
 
 
-@define
 class NegatedBranin2(Experiment):
-    properties = field(
-        factory=lambda: ExperimentProperties(
+    # All three optima are the same y_star value
+    metadata: dict[str, ArrayLike] = {
+        "x_star": _BRANIN_OPTIMA,
+        "y_star": -0.397887,
+    }
+
+    @cache
+    @property
+    def properties(self) -> ExperimentProperties:
+        return ExperimentProperties(
             n_input_dim=2,
             domain=np.array([[-5.0, 10.0], [0.0, 15.0]]).T,
         )
-    )
-    # All three optima are the same y_star value
-    metadata = field(default={"x_star": BRANIN_OPTIMA, "y_star": -0.397887})
 
     def _truth(self, x):
         x1 = x[:, 0]
         x2 = x[:, 1]
-        t1 = BRANIN_A * (x2 - BRANIN_B * x1**2 + BRANIN_C * x1 - BRANIN_R) ** 2
-        t2 = BRANIN_S * (1.0 - BRANIN_T) * np.cos(x1) + BRANIN_S
+        t1 = (
+            _BRANIN_A
+            * (x2 - _BRANIN_B * x1**2 + _BRANIN_C * x1 - _BRANIN_R) ** 2
+        )
+        t2 = _BRANIN_S * (1.0 - _BRANIN_T) * np.cos(x1) + _BRANIN_S
         y = t1 + t2
         y = -y  # negate so we can find maxima not minima
         return np.atleast_1d(y.squeeze())
